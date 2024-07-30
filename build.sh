79c8a6663674f4b953936b877ebacaaa3fcb4395
Lauren Boydstun <admin@stormyoasis.com>
2024-07-29 21:06:53 -0700

    Updating docker tags

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION=1.1.10-5
+VERSION=latest
 docker build . \
   --build-arg python_extra_flags="--single-version-externally-managed --root=/" \
-  --no-cache --tag graphiteapp/graphite-statsd:$VERSION --progress tty
+  --no-cache --tag stormyoasis/graphite-statsd-grafana:$VERSION --progress tty
